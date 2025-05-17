d5d4957d8e24dcd7c37582adbf00ddb83e794d72
tkarthikreddy <tkarthikreddy@girnarcare.com>
2025-05-17 18:43:44 +0530

    exception_logger_utilsadded

---
+++ main.py
@@ -0,0 +1,8 @@
+from mlProject import logger
+#here mlproject is the project folder name in src and logger is defind inside its file so we are able to import functionality it.
+
+#here we are not calling src but directly calling mlproject as we mentioned it already in initfile.
+#or we can mention like src.mlproject
+
+logger.info("Welcome to our custom logging")
+
